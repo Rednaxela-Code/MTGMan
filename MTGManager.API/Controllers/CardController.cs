46075ed89ffb1c0dfc2de02ecbeb96b4c8be3fda
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-10-10 20:34:05 +0200

    Mess cleaned, simple card search is working.

--- MTGManager.API/Controllers/CardController.cs
+++ MTGManager.API/Controllers/CardController.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using MTGManager.Shared.Models;
+using ScryfallApi.Client;
+
+namespace MTGManager.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CardController : ControllerBase
+    {
+        private readonly ScryfallApiClient _scryfallApi;
+
+        public CardController(ScryfallApiClient scryfallApi)
+        {
+            _scryfallApi = scryfallApi;
+        }
+
+        [HttpGet("cardsearch")]
+        public async Task<IActionResult> GetCardByFuzzy([FromQuery] string fuzzy)
+        {
+            var card = await _scryfallApi.Cards.Search(fuzzy, 0, ScryfallApi.Client.Models.SearchOptions.CardSort.Name);
+            ScryfallCard myCard = (ScryfallCard)card.Data;
+            return Ok(myCard);
+        }
+    }
+}
